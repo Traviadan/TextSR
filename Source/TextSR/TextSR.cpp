ae626c40f08dd4e535ce6b10a493600583a00dba
Thorsten Eggers <thorsten@eggers-bhv.de>
2023-11-16 16:08:47 +0100

    Initial commit

---
+++ Source/TextSR/TextSR.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TextSR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TextSR, "TextSR" );
+
+DEFINE_LOG_CATEGORY(LogTextSR)
+ 
